ddade475e8f967017bcd263c6f73ec380b9a8ee3
Robert Bryan Hughes <bryan@purr.io>
2020-02-26 14:15:06 -0600

    added new post-stackery-deploy awsmobile var

--- src/deployer/clowder/src/App.js
+++ src/deployer/clowder/src/App.js
@@ -18,7 +18,7 @@ import JobList from './components/JobList'
 /////
 /////
 /////
-const awsmobile = {
+const awsmobileOLD = {
   aws_project_region: 'us-east-2',
   aws_cognito_region: 'us-east-2',
   aws_appsync_region: 'us-east-2',
@@ -30,6 +30,20 @@ const awsmobile = {
   aws_appsync_graphqlEndpoint:
     'https://mupetkhoizhm5dqb6wxplr4vi4.appsync-api.us-east-2.amazonaws.com/graphql'
 }
+
+const awsmobile = {
+  aws_project_region: 'us-east-2',
+  aws_cognito_region: 'us-east-2',
+  aws_appsync_region: 'us-east-2',
+  aws_appsync_authenticationType: 'AWS_IAM',
+  aws_cognito_identity_pool_id:
+    'us-east-2:06a52334-6edf-4c80-8e00-51554ec085bf',
+  aws_user_pools_id: 'us-east-2_sBPeOqMdE',
+  aws_user_pools_web_client_id: '2uo8qr3kf3pdjpb4c7ra8lat2t',
+  aws_appsync_graphqlEndpoint:
+    'https://xsickjdyxfcghgum62jyhswf4q.appsync-api.us-east-2.amazonaws.com/graphql'
+}
+
 /////
 /////
 /////
